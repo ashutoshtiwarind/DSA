2ab3216d3908f1ac48d3e23f86c8d8e84197eeb4
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-16 00:10:04 +0530

    nested loop
    printing pattern of number using nested loop : for eg if n = 4 then the output should be :
    
    1 2 3 4 
    5 6 7 8
    9 10 11 12

---
+++ Loops/patternOfNumber.js
@@ -0,0 +1,12 @@
+var N = 4;
+var num = 1;
+
+for (i = 1; i <= N; i++) {
+  var str ="";
+    for (j = 1; j <= N; j++) {
+      str += num + " ";
+      num++;
+    }
+    console.log(str);
+      
+}
