dad14e8bcfbce759e377ea2549907ef044ce22cf
ashutoshtiwarind <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-15 23:57:56 +0530

    twoPrimes
    if both number are prime print True else False

---
+++ Conditional-Statements/TwoPrimes.js
@@ -0,0 +1,21 @@
+var a = 3;
+var b = 5;
+var flag_a = true;
+//var flag_b = true;
+
+for (i = 2; i < a; i++){
+  if (a % i == 0) {
+    flag_a = false;  
+  }
+}
+for (i = 2; i < b; i++){
+  if (b % i == 0) {
+    flag_a = false;  
+  }
+}
+if (flag_a) {
+  console.log("True");
+}
+else {
+  console.log("False");
+}
