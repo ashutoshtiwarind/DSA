6fd298fc483c0a27f8f4d99957e0472873d2f960
ashutoshtiwarind <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-15 23:05:03 +0530

    pattern of L
    using nested loops and if else statements to create pattern of L  * 
                                                                                                           * 
                                                                                                           * * *

---
+++ Loops/patternOfL.js
@@ -0,0 +1,17 @@
+var num = 3;
+
+for (i = 1; i <= num; i++) {
+  output = "";
+  for (j = 1; j <= num; j++) {
+    if (i == num) {
+      output = output + "*" + ' ';
+    }
+    else {
+      if (j == 1) {
+        output = output + "*" + ' ';
+      }
+    }
+  }
+   console.log(output);
+}
+
