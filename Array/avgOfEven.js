ce3c19c0d5b43f7e41d9bfcaff563532ad51eed4
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-18 23:55:32 +0530

    loop | array

---
+++ Array/avgOfEven.js
@@ -0,0 +1,14 @@
+//given numbers in an array print the average of even numbers.
+
+var numbers = [1,2,3,4,5,6,7,8,9];
+var sum = 0;
+var count = 0;
+for (i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 == 0){
+    count++;
+    sum = sum + numbers[i];
+    
+  }
+
+}
+console.log(sum/count);
