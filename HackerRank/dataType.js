ec0094e480c66b4b46fe6079ff271ff43e93072d
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-20 23:26:14 +0530

    parse Int/Float | easy

---
+++ HackerRank/dataType.js
@@ -0,0 +1,10 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    console.log(firstInteger + parseInt(secondInteger));
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
