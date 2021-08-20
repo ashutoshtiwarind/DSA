4fa73cb7067015a3f409bca837966757203763f9
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-21 00:04:58 +0530

    Update vowelAndConsonantCount.js

--- Array/vowelAndConsonantCount.js
+++ Array/vowelAndConsonantCount.js
@@ -1,15 +1,15 @@
-var str = "ash";
+var str = "aasiouph";
 var vowel_count = 0;
 var consonant_count = 0;
 
 for (i = 0; i < str.length; i++) {
   if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u") {
     vowel_count++;
-    console.log("vowel_count " + i + " " + vowel_count);
+   // console.log("vowel_count " + vowel_count);
   }
-  if (str[i] != "a" || str[i] != "e" || str[i] != "i" || str[i] != "o" || str[i] != "u") {
+  if (str[i] != "a" && str[i] != "e" && str[i] != "i" && str[i] != "o" && str[i] != "u") {
     consonant_count++;
-    console.log("consonant_count " + i + " " + consonant_count);
+    //console.log("consonant_count " + consonant_count);
   }
 }
 console.log(vowel_count + " " + consonant_count);
