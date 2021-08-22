36e0d0f172f5dc9ffed344db314de489fa94cc27
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-22 23:56:42 +0530

    Update checkPalindrome.js

--- Conditional-Statements/checkPalindrome.js
+++ Conditional-Statements/checkPalindrome.js
@@ -14,3 +14,21 @@ else{
   console.log("no");
 } 
 
+
+
+//2nd solution without using array.
+
+var N = 7;
+var str = "naamaan";
+var rev = "";
+
+for (i = N-1; i >= 0; i--) {
+  rev =rev + str[i];
+  
+}  
+if(str == rev){
+   console.log("yes");
+}
+else{
+  console.log("no");
+} 
