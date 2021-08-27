6494d05177249d17206ecac6300f5a0fd50e492c
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-08-27 21:10:28 +0530

    conditional

---
+++ function | js/avgOfArray.js
@@ -0,0 +1,17 @@
+function avgOfArray(array) {
+  var sum = 0;
+  var count = 0;
+  for (i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+    count++;
+    
+  }
+  if(array.length == 0) {
+    return 0;
+  }
+  else{
+    var avg = sum / count;
+    return avg;
+  }
+}
+ console.log(avgOfArray([]))
