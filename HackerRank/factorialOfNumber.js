ece3413f38da6a25bdf0794213966832713c4f6c
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-09-03 19:54:16 +0530

    Delete factorialOfNumber.js

--- HackerRank/factorialOfNumber.js
+++
@@ -1,9 +0,0 @@
-//Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
-function factorial(n) {
-    var fact = 1;
-    for (var i = n; i >= 1; i--) {
-        fact = fact * i;
-        
-    }    
-    return fact;        
-}
