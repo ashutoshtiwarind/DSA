bedb95c510d815960006a52a20dabfabde592a47
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-09-15 18:56:28 +0530

    Create checkPalindrome.js

---
+++ masai contest/Array/checkPalindrome.js
@@ -0,0 +1,18 @@
+var N = 4;
+var str = ["n","a","n","s"];
+
+var x = true;
+for (var i = 0; i < N / 2; i++) {
+  if (str[i] !== str[N - i - 1]) {
+
+    x = false;
+    break;
+  }
+}
+if (x){
+  console.log("Yes");
+}
+
+else{
+console.log("No");
+} 
