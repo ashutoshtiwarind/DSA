85d421bdbfaf7a115ad907f6acea0e9f957a28d1
Ashutosh Tiwari <84527888+ashutoshtiwarind@users.noreply.github.com>
2021-10-10 19:24:24 +0530

    Create CircularTraversal.js

---
+++ Array/matrix/CircularTraversal.js
@@ -0,0 +1,20 @@
+/*
+https://oj.masaischool.com/contest/2081/problem/07
+*/
+
+function cirTraversal(matrix,N) {
+    let output = "";
+    for (let r = N-1; r >= 0; r--){
+        output += matrix[r][0] + " ";
+    }
+    for (let c = 1; c < N; c++){
+        output += matrix[0][c] + " ";
+    }
+    for (let r = 1; r < N; r++) {
+        output += matrix[r][N-1] + " ";
+    }
+    for (let c = N-2; c >= 1; c--) {
+        output += matrix[N-1][c] + " ";
+    }
+    console.log(output);
+}
